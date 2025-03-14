7fec35f9d7ef33c13380280a2d189cd5b70670d8
Yanlin Feng <yanlinf23@gmail.com>
2025-03-14 22:59:14 +0000

    Default to gpt-4o

--- rattq/baseline/zero_shot_nl2q.py
+++ rattq/baseline/zero_shot_nl2q.py
@@ -23,12 +23,12 @@ Query:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--llm', default='openai/gpt-4o-mini')
+    parser.add_argument('--llm', default='openai/gpt-4o')
     parser.add_argument('--prompt', default='default', choices=['default'])
     parser.add_argument('--dataset', default='bird-sql')
     parser.add_argument('--batch_size', default=10, type=int)
     parser.add_argument('--wait_time_between_batches', default=0.0, type=float)
-    parser.add_argument('--result_dir', default='output/zero_shot_nl2q_gpt-4o-mini/')
+    parser.add_argument('--result_dir', default='output/zero_shot_nl2q_gpt-4o/')
     parser.add_argument('--overwrite', action='store_true')
     parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
