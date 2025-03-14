d61234419fce65f926cecb1d38c4abfee65b84d5
Yanlin Feng <yanlinf23@gmail.com>
2025-03-14 20:09:24 +0000

    Implement run_query

--- rattq/db_connector/base.py
+++ rattq/db_connector/base.py
@@ -1,3 +1,6 @@
 class BaseDBConnector:
     def get_schema(self):
         raise NotImplementedError()
+
+    def run_query(self, query: str, timeout: int = 30) -> list:
+        raise NotImplementedError()
--- rattq/db_connector/sql.py
+++ rattq/db_connector/sql.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from rattq.db_connector.mschema_utils import MSchema, SchemaEngine
 from rattq.db_connector.base import BaseDBConnector
 
@@ -8,8 +8,8 @@ class SQLiteConnector(BaseDBConnector):
         self.name = name
         self.db_path = db_path
 
-        self.mschema = MSchema()
         db_engine = create_engine(f'sqlite:///{db_path}')
+
         schema_engine = SchemaEngine(engine=db_engine, db_name=self.name)
         mschema = schema_engine.mschema
         mschema_str = mschema.to_mschema()
@@ -17,3 +17,8 @@ class SQLiteConnector(BaseDBConnector):
 
     def get_schema(self) -> str:
         return self._schema
+
+    def run_query(self, query: str, timeout: int = 30) -> list:
+        db_engine = create_engine(f'sqlite:///{self.db_path}', connect_args={'timeout': timeout})
+        with db_engine.connect() as conn:
+            return conn.execute(text(query))
