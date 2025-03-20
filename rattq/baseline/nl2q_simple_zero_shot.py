7a7e09e9e948776b9b36e96b30260e4f3fd911a3
Yanlin Feng <yanlinf23@gmail.com>
2025-03-20 23:10:49 +0000

    Support self-consistency voting

--- rattq/baseline/nl2q_simple_zero_shot.py
+++ rattq/baseline/nl2q_simple_zero_shot.py
@@ -4,6 +4,8 @@ import shutil
 import json
 from tqdm import trange
 import litellm
+import time
+import collections
 from rattq.utils import load_nl2q_samples, parse_query
 from rattq.db_connector import get_db_connectors
 
@@ -42,10 +44,50 @@ Query:
 """.strip()
 
 
+def is_null_result(result):
+    if not result:  # empty result
+        return True
+
+    # Check if any column is all None
+    n_cols = len(result[0])
+    for i in range(n_cols):
+        if all(row[i] is None for row in result):
+            return True
+    return False
+
+
+def select_best_query(candidates, db_connector):
+    result2query = collections.defaultdict(list)
+    run_time = {}
+    for query in candidates:
+        t0 = time.time()
+        try:
+            result = db_connector.run_query(query)
+            if is_null_result(result):
+                continue
+        except Exception as e:
+            continue
+        run_time[query] = time.time() - t0
+        hashable = tuple(
+            sorted(set(result), key=lambda row: tuple((x is None, x) for x in row))
+        )
+        result2query[hashable].append(query)
+
+    if not result2query:
+        return candidates[0]
+
+    # select majority query group
+    majority_query_group = max(result2query.values(), key=len)
+
+    # select the query with the least run time
+    return min(majority_query_group, key=lambda x: run_time[x])
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--llm", default="openai/gpt-4o")
     parser.add_argument("--temperature", default=0.0, type=float)
+    parser.add_argument("-n", "--num_majority_voting_candidates", default=1, type=int)
     parser.add_argument("--prompt", default="default", choices=["default"])
     parser.add_argument("--dataset", default="bird-sql")
     parser.add_argument("--batch_size", default=50, type=int)
@@ -89,15 +131,17 @@ def main():
     for i in trange(0, len(dev_samples), args.batch_size):
         j = min(i + args.batch_size, len(dev_samples))
         batch_samples = dev_samples[i:j]
-        prompts = [
-            NL2Q_PROMPT.format(
-                language=sample.language,
-                schema=db_connectors[sample.db].get_schema(),
-                evidence=sample.evidence,
-                question=sample.question,
-            )
-            for sample in batch_samples
-        ]
+        prompts = []
+        for sample in batch_samples:
+            prompts += [
+                NL2Q_PROMPT.format(
+                    language=sample.language,
+                    schema=db_connectors[sample.db].get_schema(),
+                    evidence=sample.evidence,
+                    question=sample.question,
+                )
+            ] * args.num_majority_voting_candidates
+
         if i == 0:
             print(f"<prompt>{prompts[0]}</prompt>")
 
@@ -107,12 +151,18 @@ def main():
             temperature=args.temperature,
             **litellm_kwargs,
         )
+        assert (
+            len(responses) == len(batch_samples) * args.num_majority_voting_candidates
+        )
         responses = [r["choices"][0]["message"]["content"] for r in responses]
+        responses = [parse_query(r) for r in responses]
         if i == 0:
             print(f"<response>{responses[0]}</response>")
-        for item, r in zip(batch_samples, responses):
-            item.pred_query = parse_query(r)
-            res.append(item)
+
+        for k, sample in enumerate(batch_samples):
+            candidates = responses[k : k + args.num_majority_voting_candidates]
+            sample.pred_query = select_best_query(candidates, db_connectors[sample.db])
+            res.append(sample)
 
     output_path = os.path.join(args.result_dir, f"result.json")
     with open(output_path, "w") as fout:
