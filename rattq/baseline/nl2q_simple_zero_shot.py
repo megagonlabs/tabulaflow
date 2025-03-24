37fc2661b227b2cb44c8371ea80fe583085b93f5
Yanlin Feng <yanlinf23@gmail.com>
2025-03-24 19:36:09 +0000

    Support metric logging, rewrite with run_llm

--- rattq/baseline/nl2q_simple_zero_shot.py
+++ rattq/baseline/nl2q_simple_zero_shot.py
@@ -6,7 +6,14 @@ from tqdm import trange
 import litellm
 import time
 import collections
-from rattq.utils import load_nl2q_samples, parse_query, is_null_result
+from concurrent.futures import ThreadPoolExecutor
+from rattq.utils import (
+    load_nl2q_samples,
+    parse_query,
+    is_null_result,
+    get_llm_api_cost,
+    save_aggregated_inference_metrics,
+)
 from rattq.db_connector import get_db_connectors
 
 NL2Q_PROMPT = """
@@ -72,6 +79,37 @@ def select_best_query(candidates, db_connector):
     return min(majority_query_group, key=lambda x: run_time[x])
 
 
+def run_llm(
+    db_connector,
+    llm: str,
+    prompt: str,
+    temperature: float = 0.0,
+    num_candidates: int = 1,
+    litellm_kwargs: dict = {},
+):
+    t0 = time.time()
+    response = litellm.batch_completion(
+        model=llm,
+        messages=[[{"role": "user", "content": prompt}] for _ in range(num_candidates)],
+        temperature=temperature,
+        **litellm_kwargs,
+    )
+    queries = [r["choices"][0]["message"]["content"] for r in response]
+    queries = [parse_query(q) for q in queries]
+    input_tokens = sum([r["usage"]["prompt_tokens"] for r in response])
+    output_tokens = sum([r["usage"]["completion_tokens"] for r in response])
+    api_cost_usd = get_llm_api_cost(llm, input_tokens, output_tokens)
+    latency = time.time() - t0
+    metrics = {
+        "latency": round(latency, 1),
+        "input_tokens": int(input_tokens),
+        "output_tokens": int(output_tokens),
+        "api_cost_usd": api_cost_usd,
+    }
+    best_query = select_best_query(queries, db_connector)
+    return best_query, metrics
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--llm", default="openai/gpt-4o")
@@ -120,44 +158,48 @@ def main():
     for i in trange(0, len(dev_samples), args.batch_size):
         j = min(i + args.batch_size, len(dev_samples))
         batch_samples = dev_samples[i:j]
-        prompts = []
-        for sample in batch_samples:
-            prompts += [
-                NL2Q_PROMPT.format(
-                    language=sample.language,
-                    schema=db_connectors[sample.db].get_schema(),
-                    evidence=sample.evidence,
-                    question=sample.question,
-                )
-            ] * args.num_majority_voting_candidates
-
+        prompts = [
+            NL2Q_PROMPT.format(
+                language=item.language,
+                schema=db_connectors[item.db].get_schema(),
+                evidence=item.evidence,
+                question=item.question,
+            )
+            for item in batch_samples
+        ]
         if i == 0:
             print(f"<prompt>{prompts[0]}</prompt>")
 
-        responses = litellm.batch_completion(
-            model=args.llm,
-            messages=[[{"role": "user", "content": s}] for s in prompts],
-            temperature=args.temperature,
-            **litellm_kwargs,
-        )
-        assert (
-            len(responses) == len(batch_samples) * args.num_majority_voting_candidates
-        )
-        responses = [r["choices"][0]["message"]["content"] for r in responses]
-        responses = [parse_query(r) for r in responses]
-        if i == 0:
-            print(f"<response>{responses[0]}</response>")
+        with ThreadPoolExecutor(max_workers=len(prompts)) as executor:
+            futures = [
+                executor.submit(
+                    run_llm,
+                    db_connectors[item.db],
+                    args.llm,
+                    prompt,
+                    args.temperature,
+                    args.num_majority_voting_candidates,
+                    litellm_kwargs,
+                )
+                for item, prompt in zip(batch_samples, prompts)
+            ]
+            raw_responses = [future.result() for future in futures]
 
-        for k, sample in enumerate(batch_samples):
-            n = args.num_majority_voting_candidates
-            candidates = responses[k * n : (k + 1) * n]
-            sample.pred_query = select_best_query(candidates, db_connectors[sample.db])
-            res.append(sample)
+        if i == 0:
+            print(f"<response>{raw_responses[0][0]}</response>")
+
+        for item, r in zip(batch_samples, raw_responses):
+            query, metrics = r
+            item.pred_query = query
+            item.metrics.update(metrics)
+            res.append(item)
 
     output_path = os.path.join(args.result_dir, f"result.json")
     with open(output_path, "w") as fout:
         json.dump([item.model_dump(mode="json") for item in res], fout, indent=2)
     print(f"Saved result to {output_path}")
+
+    save_aggregated_inference_metrics([item.metrics for item in res], args.result_dir)
 
 
 if __name__ == "__main__":
