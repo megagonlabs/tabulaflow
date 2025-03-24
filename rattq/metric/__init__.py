471cec9e3b7954db726e5ba799281447ea504852
Yanlin Feng <yanlinf23@gmail.com>
2025-03-24 00:31:08 +0000

    Add metrics gold_executable and gold_not_null

--- rattq/metric/__init__.py
+++ rattq/metric/__init__.py
@@ -1,5 +1,13 @@
 from rattq.metric.bird_sql_ex import bird_sql_ex
 from rattq.metric.bird_sql_ex_soft import bird_sql_ex_soft
 from rattq.metric.executable import executable
+from rattq.metric.gold_executable import gold_executable
+from rattq.metric.gold_not_null import gold_not_null
 
-__all__ = ["bird_sql_ex", "bird_sql_ex_soft", "executable"]
+__all__ = [
+    "bird_sql_ex",
+    "bird_sql_ex_soft",
+    "executable",
+    "gold_executable",
+    "gold_not_null",
+]
