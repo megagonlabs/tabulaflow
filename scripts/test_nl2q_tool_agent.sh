36163760f29176e97597a006deac3c2e4b81954a
Yanlin Feng <yanlinf23@gmail.com>
2025-03-28 21:17:50 +0000

    Minor

--- scripts/test_nl2q_tool_agent.sh
+++ scripts/test_nl2q_tool_agent.sh
@@ -1,2 +1,2 @@
-python -m rattq.baseline.nl2q_tool_agent --debug --result_dir output/test/
+python -m rattq.baseline.nl2q_tool_agent --debug --result_dir output/test/ --llm openai/gpt-4o
 python -m rattq.evaluate --result_dir output/test/
