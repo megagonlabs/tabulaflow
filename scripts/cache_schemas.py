90f38770cb4f666097e9b7e1dfde074a4de1000d
Yanlin Feng <yanlinf23@gmail.com>
2025-05-05 19:18:56 +0000

    cache_schemas.py

---
+++ scripts/cache_schemas.py
@@ -0,0 +1,24 @@
+import argparse
+import time
+from mintq.dataset import get_dataset_loader
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default="spider2-snow")
+    parser.add_argument("--split", default="test")
+    parser.add_argument("--databases", default=None, nargs="+")
+    args = parser.parse_args()
+    print(args)
+    print()
+
+    t0 = time.time()
+    dataset_loader = get_dataset_loader(args.dataset)
+    dataset = dataset_loader.get_split(args.split, databases=args.databases)
+    print(
+        f"Loaded {len(dataset.tasks)} samples and {len(dataset.db_connectors)} databases from {args.dataset} {args.split} set in {time.time() - t0:.2f} seconds."
+    )
+
+
+if __name__ == "__main__":
+    main()
