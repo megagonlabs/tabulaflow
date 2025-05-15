a0714dab4cdc2abc8a5c41d4d3c5f4c68bf0f9ec
Yanlin Feng <yanlinf23@gmail.com>
2025-05-15 23:49:17 +0000

    Refacotr schema formmater

--- mintq/schema_formatter/base.py
+++ mintq/schema_formatter/base.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
-from mintq.schema import BaseDBSchema
+from mintq.schema import SQLSchema
 
 
 class BaseSchemaFormatter(ABC):
     name: str
 
+
+class BaseSQLSchemaFormatter(BaseSchemaFormatter):
+    name: str
+
     @abstractmethod
-    def format(self, schema: BaseDBSchema) -> str:
+    def format(self, schema: SQLSchema) -> str:
         pass
