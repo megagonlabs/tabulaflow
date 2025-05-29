acbb2ea32ebb248331adc53b43393085a6c58d17
Yanlin Feng <yanlinf23@gmail.com>
2025-05-29 04:48:23 +0000

    Fix model name

--- scripts/create_exp.sh
+++ scripts/create_exp.sh
@@ -11,10 +11,10 @@ model=$1
 dataset=$2
 llm=${3:-"openai/gpt-4o"}
 
-# if model="agent", set model to "sql_agent_table_names_only"
+# if model="agent", set model to "sql_agent"
 # if model="zero", set model to "simple_zero_shot"
 if [ "$model" = "agent" ]; then
-    model="sql_agent_table_names_only"
+    model="sql_agent"
 elif [ "$model" = "zero" ]; then
     model="simple_zero_shot"
 fi
@@ -53,7 +53,7 @@ exp_file="exp/${exp_name}.sh"
 cat <<EOF >$exp_file
 set -e
 
-.venv/bin/python -u -m mintq.run_model --model $model --dataset $dataset --llm $llm --result_dir output/${exp_name}/ --overwrite
+.venv/bin/python -u -m mintq.run_model --model $model --dataset $dataset --llm $llm --result_dir output/${exp_name}/ --batch_size 500 --overwrite
 .venv/bin/python -u -m mintq.evaluate --result_json output/${exp_name}/result.json
 EOF
 
