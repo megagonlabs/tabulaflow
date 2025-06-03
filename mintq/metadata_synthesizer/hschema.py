e58d1e10097c3a8df86ee774fe539f70f425a456
Yanlin Feng <yanlinf23@gmail.com>
2025-06-03 04:48:12 +0000

    Concurrent table processing, initial HSchemaSynthesizer

--- mintq/metadata_synthesizer/hschema.py
+++ mintq/metadata_synthesizer/hschema.py
@@ -1,10 +1,15 @@
+import asyncio
 from dataclasses import dataclass
 from mintq.schema import (
     SQLTableSchema,
     HTableSection,
     HColumnGroup,
+    HSQLSchema,
+    HTableGroup,
+    HTableSchema,
 )
 from mintq.metadata_synthesizer.llm_clusterer import LLMClusterer
+from mintq.db_connector import BaseAsyncSQLDBConnector
 
 
 SECTION_PROMPT = """
@@ -46,3 +51,44 @@ class HTableSchemaSynthesizer:
             )
             for s in sections
         ]
+
+    async def run(self, table: SQLTableSchema) -> HTableSchema:
+        sections = await self.build_sections(table)
+
+        return HTableSchema(
+            name=table.name,
+            schema_name=table.schema_name,
+            primary_key=table.primary_key,
+            num_rows=table.num_rows,
+            sections=sections,
+        )
+
+
+@dataclass
+class HSchemaSynthesizer:
+    llm: str = "gpt-4o"
+    batch_size: int = 20
+    temperature: float = 0.0
+
+    async def run(self, db_connector: BaseAsyncSQLDBConnector) -> HSQLSchema:
+        schema = db_connector.schema
+
+        table_synthesizer = HTableSchemaSynthesizer(
+            llm=self.llm,
+            batch_size=self.batch_size,
+            temperature=self.temperature,
+        )
+
+        table_hschemas = await asyncio.gather(*[table_synthesizer.run(table) for table in schema.tables])
+
+        return HSQLSchema(
+            name=schema.name,
+            table_groups=[
+                HTableGroup(
+                    name=table.name,
+                    tables=[table],
+                )
+                for table in table_hschemas
+            ],
+            foreign_keys=schema.foreign_keys,
+        )
