f88bb32f93c510ad07be216f0273f2730d3e59e0
Yanlin Feng <yanlinf23@gmail.com>
2025-06-08 00:51:53 +0000

    Format PK and FKs

--- mintq/formatters/sql.py
+++ mintq/formatters/sql.py
@@ -9,8 +9,6 @@ class SQLDefaultSchemaFormatter:
     name: ClassVar[str] = "sql_default"
     quote_char: str = '"'
     example_max_chars: int = 100
-    include_foreign_keys: bool = True
-
 
     def _quote(self, s: str) -> str:
         return f"{self.quote_char}{s}{self.quote_char}"
@@ -32,16 +30,18 @@ class SQLDefaultSchemaFormatter:
     def format_table_name(self, table: SQLTableSchema) -> str:
         return self._full_table_name(table.name, table.schema_name)
 
-    def format(self, schema: SQLSchema, include_foreign_keys: bool = True, include_table_schemas: bool = True) -> str:
+    def format(self, schema: SQLSchema) -> str:
         res = f"Database: {schema.name}"
         res += "\n\n"
         res += "\n\n".join([self.format_table(table) for table in schema.tables])
         return res
 
     def format_table(self, table: SQLTableSchema) -> str:
+        for fk in table.foreign_keys:
+            if len(fk.columns) > 1:
+                raise NotImplementedError(f"Composite foreign keys are not supported: {fk.columns} in TABLE{table.name}")
+
         res = f"=== TABLE: {self._full_table_name(table.name, table.schema_name)} ===\n"
-        if table.primary_key:
-            res += f"Primary key: {', '.join([self._quote_if_needed(pk) for pk in table.primary_key])}\n"
         res += "\n".join([self.format_column(column) for column in table.columns])
         res += "\n=== END OF TABLE ==="
         return res
@@ -49,9 +49,7 @@ class SQLDefaultSchemaFormatter:
     def format_column(self, column: SQLColumnSchema) -> str:
         res = f"- {self._quote_if_needed(column.name)}: {column.dtype}"
         is_categorical = (
-            column.dtype in ("TEXT", "VARCHAR")
-            and 0 < column.num_unique <= 20
-            and column.unique_ratio < 0.01
+            column.dtype in ("TEXT", "VARCHAR") and 0 < column.num_unique <= 20 and column.unique_ratio < 0.01
         )
         if is_categorical:  # show all possible values
             valid_values = [self._quote(self._truncate(v)) for v in column.examples]
@@ -69,4 +67,14 @@ class SQLDefaultSchemaFormatter:
                 example = str(example)
             example = self._truncate(example)
             res += f" (e.g. {example})"
+
+        if column.primary_key_type:
+            res += " [PK]" if column.primary_key_type == "single" else " [PK-composite]"
+        for fk in column.foreign_keys:
+            is_composite_fk = len(fk.columns) > 1
+            res += (
+                f" [FK: -> {self._full_table_name(fk.foreign_table, fk.foreign_schema_name)}.{self._quote_if_needed(fk.foreign_columns[0])}]"
+                if not is_composite_fk
+                else " [FK-composite]"
+            )
         return res
