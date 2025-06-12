7a9985d5abce5ff9ab7fb25733179b62e7855a5b
Yanlin Feng <yanlinf23@gmail.com>
2025-06-12 22:01:06 +0000

    Remove snowflake_conn.py

--- mintq/db_connector/__init__.py
+++ mintq/db_connector/__init__.py
@@ -1,10 +1,8 @@
 from mintq.db_connector.base import BaseAsyncDBConnector, BaseAsyncSQLDBConnector
-from mintq.db_connector.snowflake_conn import SnowflakeConnector
 from mintq.db_connector.sql_conn import SQLConnector
 
 __all__ = [
     "BaseAsyncDBConnector",
     "BaseAsyncSQLDBConnector",
     "SQLConnector",
-    "SnowflakeConnector",
 ]
