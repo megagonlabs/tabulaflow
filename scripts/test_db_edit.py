5ca7019ecb8eecbffa91f197c5b404e3cfcb199e
Yanlin Feng <yanlinf23@gmail.com>
2025-06-25 18:58:51 +0000

    Fix

--- scripts/test_db_edit.py
+++ scripts/test_db_edit.py
@@ -61,6 +61,7 @@ def create_db(db_path: str):
 async def main():
     os.environ["MINTQ_CACHE_ENABLED"] = "0"
     db_path = "output/test.db"
+    create_db(db_path)
     db_connector = await SQLConnector.from_url_async(
         "test+city_stats",
         "city_stats",
