18070b8887d4663d1ca8c749b104bf64b92fab1d
Yanlin Feng <yanlinf23@gmail.com>
2025-07-08 20:09:39 +0000

    pprint_hschema.py

---
+++ scripts/pprint_hschema.py
@@ -0,0 +1,27 @@
+import asyncio
+import argparse
+import os
+from mintq.formatters import HSchemaFormatter
+from mintq.schema import HSQLSchema
+
+
+async def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_path", default="cache/hschema/bird-sql+european_football_2.json")
+    args = parser.parse_args()
+    print(args)
+    print()
+
+    if not os.path.exists(args.input_path):
+        raise FileNotFoundError(f"File {args.input_path} does not exist")
+
+    with open(args.input_path, "r") as f:
+        hschema = HSQLSchema.model_validate_json(f.read())
+
+    formatter = HSchemaFormatter()
+    hschema_str = formatter.format(hschema)
+    print(hschema_str)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
