ca43b8cc479d9c580ab2609b24bd0226f85fef2b
Yanlin Feng <yanlinf23@gmail.com>
2025-07-10 20:26:04 +0000

    Add new stat ratio_columns_with_description

--- scripts/print_dataset_stats.py
+++ scripts/print_dataset_stats.py
@@ -21,16 +21,20 @@ async def main() -> None:
         f"Loaded {len(dataset.tasks)} samples and {len(dataset.db_connectors)} databases from {args.dataset} {args.split} set in {time.time() - t0:.2f} seconds."
     )
 
-    per_db_stats = {"database": [], "num_tables": [], "num_columns": []}  # type: ignore
+    per_db_stats = {"database": [], "num_tables": [], "num_columns": [], "ratio_columns_with_description": []}  # type: ignore
     db_names = sorted(dataset.db_connectors.keys())
     for db_name in db_names:
         schema = dataset.db_connectors[db_name].schema
         per_db_stats["database"].append(db_name)
         per_db_stats["num_tables"].append(len(schema.tables))
         per_db_stats["num_columns"].append(sum(len(table.columns) for table in schema.tables))
+        per_db_stats["ratio_columns_with_description"].append(
+            sum(1 for table in schema.tables for column in table.columns if column.description is not None)
+            / per_db_stats["num_columns"][-1]
+        )
     print()
     print("### Per-Database Stats")
-    print(tabulate(per_db_stats, headers=list(per_db_stats.keys()), tablefmt=args.format))
+    print(tabulate(per_db_stats, headers=list(per_db_stats.keys()), tablefmt=args.format, floatfmt=".2f"))
 
     aggregated_stats = {
         "dataset": args.dataset,
@@ -44,6 +48,8 @@ async def main() -> None:
         "avg_columns_per_db": sum(per_db_stats["num_columns"]) / len(dataset.db_connectors),
         "min_columns_per_db": min(per_db_stats["num_columns"]),
         "avg_columns_per_table": sum(per_db_stats["num_columns"]) / sum(per_db_stats["num_tables"]),
+        "avg_ratio_columns_with_description": sum(per_db_stats["ratio_columns_with_description"])
+        / len(dataset.db_connectors),
     }
     print()
     print("### Aggregated Stats")
