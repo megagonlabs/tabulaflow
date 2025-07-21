b32fd5f35c79da26ab074ba7b96fb5fffecf22f8
Yanlin Feng <yanlinf23@gmail.com>
2025-07-21 01:06:56 +0000

    Add test_index_affix_cluster_func and test_year_affix_cluster_func

--- tests/test_cluster_func.py
+++ tests/test_cluster_func.py
@@ -1,5 +1,35 @@
 import collections
-from mintq.metadata_synthesizer.clusterer import YearMonthAffixClusterFunc
+from mintq.metadata_synthesizer.clusterer import IndexAffixClusterFunc, YearAffixClusterFunc, YearMonthAffixClusterFunc
+
+
+def test_index_affix_cluster_func() -> None:
+    test_cases = [["A1", "A2", "A3", "A5"]]
+    for test_case in test_cases:
+        fn = IndexAffixClusterFunc(max_missing_ratio=0.2)
+        groups = collections.defaultdict(list)
+        for name in test_case:
+            pattern, variation = fn.extract(name)
+            groups[pattern].append(variation)
+
+        assert len(groups) == 1
+        desc = fn.summarize([v for v in groups[list(groups.keys())[0]]])
+        assert desc == "# from 1 to 5 except 4"
+
+
+def test_year_affix_cluster_func() -> None:
+    test_cases = [
+        ["2021", "2022", "2024", "2025"],
+    ]
+    for test_case in test_cases:
+        fn = YearAffixClusterFunc(max_missing_ratio=0.2)
+        groups = collections.defaultdict(list)
+        for name in test_case:
+            pattern, variation = fn.extract(name)
+            groups[pattern].append(variation)
+
+        assert len(groups) == 1
+        desc = fn.summarize([v for v in groups[list(groups.keys())[0]]])
+        assert desc == "YEAR from 2021 to 2025 except 2023"
 
 
 def test_year_month_affix_cluster_func() -> None:
@@ -25,7 +55,7 @@ def test_year_month_affix_cluster_func() -> None:
     ]
 
     for test_case in test_cases:
-        fn = YearMonthAffixClusterFunc()
+        fn = YearMonthAffixClusterFunc(max_missing_ratio=0.2)
         groups = collections.defaultdict(list)
         for name in test_case:
             pattern, variation = fn.extract(name)
