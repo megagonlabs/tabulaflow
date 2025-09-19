24a52f37394259bde11d95b158a14b4f4714b3f9
Yanlin Feng <yanlinf23@gmail.com>
2025-09-19 22:31:14 +0000

    test_func_timeout

--- scripts/test_timeout.py
+++ scripts/test_timeout.py
@@ -1,10 +1,16 @@
 import os
 import asyncio
 import time
+import func_timeout
+import sqlite3
 from mintq.db_connector import SQLConnector
 
 
 # os.environ["MINTQ_CACHE_ENABLED"] = "0"
+
+SQLITE_PATH = "data/ARCS/databases/retails.sqlite"
+SHORT_QUERY = "SELECT COUNT(*) FROM lineitem l1"
+LONG_QUERY = "SELECT COUNT(DISTINCT l1.l_orderkey) FROM lineitem l1, lineitem l2"
 
 
 async def test_timeout_aiosqlite() -> None:
@@ -13,17 +19,13 @@ async def test_timeout_aiosqlite() -> None:
         global_id="arcs+retails",
         db_name="retails",
         engine_type="async",
-        url="sqlite+aiosqlite:///data/ARCS/databases/retails.sqlite",
+        url=f"sqlite+aiosqlite:///{SQLITE_PATH}",
         max_concurrency_per_db=4,
     )
     t0 = time.time()
-    df = await connector.run_query_async(
-        "SELECT COUNT(*) FROM lineitem l1", return_df=True, timeout=5
-    )
+    df = await connector.run_query_async(SHORT_QUERY, return_df=True, timeout=5)
     print(df.head())
-    df = await connector.run_query_async(
-        "SELECT COUNT(DISTINCT l1.l_orderkey) FROM lineitem l1, lineitem l2", return_df=True, timeout=5
-    )
+    df = await connector.run_query_async(LONG_QUERY, return_df=True, timeout=5)
     print(df.head())
     print(f"Finished in {time.time() - t0:.2f} seconds")
 
@@ -34,20 +36,29 @@ async def test_timeout_sqlite() -> None:
         global_id="arcs+retails",
         db_name="retails",
         engine_type="sync",
-        url="sqlite:///data/ARCS/databases/retails.sqlite",
+        url=f"sqlite:///{SQLITE_PATH}",
         max_concurrency_per_db=4,
     )
     t0 = time.time()
-    df = await connector.run_query_async(
-        "SELECT COUNT(*) FROM lineitem l1", return_df=True, timeout=5
-    )
+    df = await connector.run_query_async(SHORT_QUERY, return_df=True, timeout=5)
     print(df.head())
-    df = await connector.run_query_async(
-        "SELECT COUNT(DISTINCT l1.l_orderkey) FROM lineitem l1, lineitem l2", return_df=True, timeout=5
-    )
+    df = await connector.run_query_async(LONG_QUERY, return_df=True, timeout=5)
     print(df.head())
     print(f"Finished in {time.time() - t0:.2f} seconds")
 
 
+def run_query(sqlite_path, sql):
+    with sqlite3.connect(sqlite_path) as conn:
+        return conn.execute(sql).fetchall()
+
+
+async def test_func_timeout() -> None:
+    print("Testing test_func_timeout...")
+    rows = func_timeout.func_timeout(5, lambda: run_query(SQLITE_PATH, SHORT_QUERY))
+    print(rows[:5])
+    rows = func_timeout.func_timeout(5, lambda: run_query(SQLITE_PATH, LONG_QUERY))
+    print(rows[:5])
+
+
 if __name__ == "__main__":
-    asyncio.run(test_timeout_aiosqlite())
+    asyncio.run(test_func_timeout())
