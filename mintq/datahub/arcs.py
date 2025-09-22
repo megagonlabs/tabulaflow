9f2325cd261804cf76286cc44228e4877df74612
Yanlin Feng <yanlinf23@gmail.com>
2025-09-22 23:03:50 +0000

    ARCS task loading

--- mintq/datahub/arcs.py
+++ mintq/datahub/arcs.py
@@ -3,12 +3,13 @@ import json
 import random
 import asyncio
 from typing import Optional, Any, Literal
-from mintq.schema import SimpleNL2QTask, NL2QDataset
+from mintq.schema import AmbigNL2QTask, NL2QDataset
 from mintq.db_connector import SQLConnector
 
 
 class ARCSDatasetLoader:
     name = "arcs"
+    splits = ["dev"]
 
     def __init__(
         self,
@@ -20,10 +21,10 @@ class ARCSDatasetLoader:
         self._split_data: dict[str, NL2QDataset] = {}
         self._dbms_semaphore = asyncio.Semaphore(1)
 
-    async def _load_tasks_async(self, split_id: str) -> list[SimpleNL2QTask]:
-        return []
+    async def _load_tasks_async(self, split: str) -> list[AmbigNL2QTask]:
+        return [AmbigNL2QTask.from_directory(os.path.join(self.directory, "tasks", f"{i:03d}")) for i in range(1, 102)]
 
-    async def _load_databases_async(self, databases: list[str]) -> dict[str, SQLConnector]:
+    async def _load_databases_async(self, split: str, databases: list[str]) -> dict[str, SQLConnector]:
         db_connectors = await asyncio.gather(
             *[
                 SQLConnector.from_url_async(
@@ -50,19 +51,22 @@ class ARCSDatasetLoader:
         ]
 
     async def get_split_async(
-        self, split_id: str, databases: Optional[list[str]] = None, database_only: bool = False
+        self, split: str, databases: Optional[list[str]] = None, database_only: bool = False
     ) -> NL2QDataset:
-        if split_id not in self._split_data:
-            self._split_data[split_id] = NL2QDataset(
+        if split not in self.splits:
+            raise ValueError(f"Split {split} not supported, only {self.splits} are supported for {self.name}")
+
+        if split not in self._split_data:
+            self._split_data[split] = NL2QDataset(
                 name=self.name,
-                split_id=split_id,
+                split=split,
                 tasks=[],
                 db_connectors={},
             )
 
-        dataset = self._split_data[split_id]
+        dataset = self._split_data[split]
         if not dataset.tasks and not database_only:
-            dataset.tasks = await self._load_tasks_async(split_id)
+            dataset.tasks = await self._load_tasks_async(split)
 
         if databases is None:
             databases = await self._get_all_databases_async()
@@ -72,7 +76,7 @@ class ARCSDatasetLoader:
 
         return NL2QDataset(
             name=self.name,
-            split_id=split_id,
+            split=split,
             tasks=dataset.tasks,
             db_connectors={db: dataset.db_connectors[db] for db in databases},
         )
