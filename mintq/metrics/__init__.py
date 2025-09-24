94f0eb153adc86b4a9ec166eea95a1c3b1586901
Yanlin Feng <yanlinf23@gmail.com>
2025-09-24 00:18:19 +0000

    Rename package: metric -> metrics

--- mintq/metrics/__init__.py
+++ mintq/metrics/__init__.py
@@ -1,11 +1,11 @@
 from typing import Any
-from mintq.metric.base import BaseAsyncNL2QMetric
-from mintq.metric.bird_sql_ex import BirdSQLEx
-from mintq.metric.bird_sql_ex_soft import BirdSQLExSoft
-from mintq.metric.executable import Executable
-from mintq.metric.gold_executable import GoldExecutable
-from mintq.metric.gold_result_not_empty import GoldResultNotEmpty
-from mintq.metric.spider2_ex import Spider2Ex
+from mintq.metrics.base import BaseAsyncNL2QMetric
+from mintq.metrics.bird_sql_ex import BirdSQLEx
+from mintq.metrics.bird_sql_ex_soft import BirdSQLExSoft
+from mintq.metrics.executable import Executable
+from mintq.metrics.gold_executable import GoldExecutable
+from mintq.metrics.gold_result_not_empty import GoldResultNotEmpty
+from mintq.metrics.spider2_ex import Spider2Ex
 
 __all__ = [
     "BaseAsyncNL2QMetric",
--- mintq/pipelines/evaluate.py
+++ mintq/pipelines/evaluate.py
@@ -10,7 +10,7 @@ from mintq.db_connector import BaseAsyncDBConnector
 from mintq.schema import NL2QTaskOutput, NL2QRunResult, NL2QDataset
 from mintq.utils import avg_and_round, save_csv
 from mintq.datahub import get_dataset_loader
-from mintq.metric import get_metric, BaseAsyncNL2QMetric
+from mintq.metrics import get_metric, BaseAsyncNL2QMetric
 from mintq.toolhub.utils import format_df
 
 
