6003fd1c114eec651b75f8f42dac299b6aa1a568
Yanlin Feng <yanlinf23@gmail.com>
2025-09-25 21:40:17 +0000

    Remove sql_agent_v2

--- mintq/agenthub/__init__.py
+++ mintq/agenthub/__init__.py
@@ -1,11 +1,10 @@
 from mintq.agenthub.simple_zero_shot import SimpleZeroShotNL2Q
 from mintq.agenthub.base import BaseAsyncNL2QAgent
 from mintq.agenthub.sql_agent import SQLAgent
-from mintq.agenthub.sql_agent_v2 import SQLAgentV2
 from mintq.agenthub.sql_multi_agent_v1 import SQLMultiAgentV1
 from mintq.agenthub.ambig_simple import AmbigSimpleSQLAgent
 
-all_model_classes = [SimpleZeroShotNL2Q, SQLAgent, SQLAgentV2, SQLMultiAgentV1, AmbigSimpleSQLAgent]
+all_model_classes = [SimpleZeroShotNL2Q, SQLAgent, SQLMultiAgentV1, AmbigSimpleSQLAgent]
 
 model_registry: dict[str, type[BaseAsyncNL2QAgent]] = {cls.name: cls for cls in all_model_classes}  # type: ignore
 
@@ -21,7 +20,6 @@ __all__ = [
     "BaseAsyncNL2QAgent",
     "SimpleZeroShotNL2Q",
     "SQLAgent",
-    "SQLAgentV2",
     "SQLMultiAgentV1",
     "AmbigSimpleSQLAgent",
 ]
