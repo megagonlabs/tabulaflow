68deffd24a6880565ced5d02fe509b3ea92e3109
Yanlin Feng <yanlinf23@gmail.com>
2025-09-26 19:28:15 +0000

    Make mypy happy

--- mintq/registry.py
+++ mintq/registry.py
@@ -1,13 +1,22 @@
+from typing import Protocol, Type
+
+
+class NamedClass(Protocol):
+    name: str
+
+
 class Registry:
     def __init__(self, registry_name: str):
         self.registry_name = registry_name
-        self._name_to_cls = {}
+        self._name_to_cls: dict[str, Type[NamedClass]] = {}
 
-    def register(self, cls: type):
+    def register(self, cls: Type[NamedClass]) -> Type[NamedClass]:
+        if cls.name in self._name_to_cls:
+            raise ValueError(f"Class {cls.name} already registered")
         self._name_to_cls[cls.name] = cls
         return cls
 
-    def get_class(self, name: str) -> type:
+    def get_class(self, name: str) -> Type[NamedClass]:
         if name not in self._name_to_cls:
             raise ValueError(f"Unknown {self.registry_name} {name}, available: {self.list_names()}")
         return self._name_to_cls[name]
