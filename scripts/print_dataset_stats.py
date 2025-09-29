e8f2bd3e5fc3f7e65f5752983c09c8a3794913fd
Yanlin Feng <yanlinf23@gmail.com>
2025-09-29 23:45:07 +0000

    Show compresse stats in the same table

--- scripts/print_dataset_stats.py
+++ scripts/print_dataset_stats.py
@@ -12,7 +12,6 @@ async def main() -> None:
     parser.add_argument("--dataset", default="spider2-snow")
     parser.add_argument("--split", default="dev")
     parser.add_argument("--format", default="github")
-    parser.add_argument("--compress", action="store_true")
     parser.add_argument("--no_cache", action="store_true")
     args = parser.parse_args()
     print(args)
@@ -28,18 +27,26 @@ async def main() -> None:
         f"Loaded {len(dataset.tasks)} samples and {len(dataset.db_connectors)} databases from {args.dataset} {args.split} set in {time.time() - t0:.2f} seconds."
     )
 
-    per_db_stats = {"database": [], "num_tables": [], "num_columns": [], "ratio_columns_with_description": []}  # type: ignore
+    per_db_stats = {
+        "database": [],
+        "tables": [],
+        "tables_compressed": [],
+        "columns": [],
+        "columns_compressed": [],
+        "ratio_columns_with_desc": [],
+    }  # type: ignore
     db_names = sorted(dataset.db_connectors.keys())
     for db_name in db_names:
         schema = dataset.db_connectors[db_name].schema
-        if args.compress:
-            schema = await SchemaCompressor().run_async(schema)
+        compressed_schema = await SchemaCompressor().run_async(schema)
         per_db_stats["database"].append(db_name)
-        per_db_stats["num_tables"].append(len(schema.tables))
-        per_db_stats["num_columns"].append(sum(len(table.columns) for table in schema.tables))
-        per_db_stats["ratio_columns_with_description"].append(
+        per_db_stats["tables"].append(len(schema.tables))
+        per_db_stats["tables_compressed"].append(len(compressed_schema.tables))
+        per_db_stats["columns"].append(sum(len(table.columns) for table in schema.tables))
+        per_db_stats["columns_compressed"].append(sum(len(table.columns) for table in compressed_schema.tables))
+        per_db_stats["ratio_columns_with_desc"].append(
             sum(1 for table in schema.tables for column in table.columns if column.description is not None)
-            / per_db_stats["num_columns"][-1]
+            / per_db_stats["columns"][-1]
         )
     print()
     print("### Per-Database Stats")
@@ -50,14 +57,20 @@ async def main() -> None:
         "split": args.split,
         "total_tasks": len(dataset.tasks),
         "total_databases": len(dataset.db_connectors),
-        "max_tables_per_db": max(per_db_stats["num_tables"]),
-        "avg_tables_per_db": sum(per_db_stats["num_tables"]) / len(dataset.db_connectors),
-        "min_tables_per_db": min(per_db_stats["num_tables"]),
-        "max_columns_per_db": max(per_db_stats["num_columns"]),
-        "avg_columns_per_db": sum(per_db_stats["num_columns"]) / len(dataset.db_connectors),
-        "min_columns_per_db": min(per_db_stats["num_columns"]),
-        "avg_columns_per_table": sum(per_db_stats["num_columns"]) / sum(per_db_stats["num_tables"]),
-        "avg_ratio_columns_with_description": sum(per_db_stats["ratio_columns_with_description"])
+        "max_tables_per_db": max(per_db_stats["tables"]),
+        "avg_tables_per_db": sum(per_db_stats["tables"]) / len(dataset.db_connectors),
+        "min_tables_per_db": min(per_db_stats["tables"]),
+        "max_tables_compressed_per_db": max(per_db_stats["tables_compressed"]),
+        "avg_tables_compressed_per_db": sum(per_db_stats["tables_compressed"]) / len(dataset.db_connectors),
+        "min_tables_compressed_per_db": min(per_db_stats["tables_compressed"]),
+        "max_columns_per_db": max(per_db_stats["columns"]),
+        "avg_columns_per_db": sum(per_db_stats["columns"]) / len(dataset.db_connectors),
+        "min_columns_per_db": min(per_db_stats["columns"]),
+        "max_columns_compressed_per_db": max(per_db_stats["columns_compressed"]),
+        "avg_columns_compressed_per_db": sum(per_db_stats["columns_compressed"]) / len(dataset.db_connectors),
+        "min_columns_compressed_per_db": min(per_db_stats["columns_compressed"]),
+        "avg_columns_per_table": sum(per_db_stats["columns"]) / sum(per_db_stats["tables"]),
+        "avg_ratio_columns_with_desc": sum(per_db_stats["ratio_columns_with_desc"])
         / len(dataset.db_connectors),
     }
     print()
