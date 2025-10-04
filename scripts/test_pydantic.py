2290ff8e37f0ed28ef0fc3236adc8e583028170e
Yanlin Feng <yanlinf23@gmail.com>
2025-10-04 03:18:49 +0000

    test_pydantic.py

--- scripts/test_pydantic.py
+++ scripts/test_pydantic.py
@@ -7,9 +7,10 @@ class Parent(BaseModel):
 
 class Child(Parent):
     b: int
+    xs: list[str]
 
 
-child = Child(a=1, b=2)
+child = Child(a=1, b=2, xs=["a", "b"])
 print(child.model_dump_json(indent=2))
 print(Parent.model_dump_json(child, indent=2))
 print(Child.model_dump_json(child, indent=2, exclude={"b"}))
@@ -18,3 +19,6 @@ print(Child.model_dump_json(child, indent=2, exclude={"b"}))
 print(Parent.model_validate(child))
 print(type(Parent.model_validate(child)))
 print(Parent(**child.model_dump()))
+
+
+print(type(child).model_fields["xs"].annotation == list[str])
