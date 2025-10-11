c703c9720abfd7fe20c18a4c99909d5ae4eea227
Yanlin Feng <yanlinf23@gmail.com>
2025-10-11 01:16:46 +0000

    Fix bug

--- mintq/agenthub/__init__.py
+++ mintq/agenthub/__init__.py
@@ -5,20 +5,22 @@ from mintq.agenthub.base import (
     NL2QAgent,
     agent_registry,
 )
-from mintq.agenthub.simple_zero_shot import SimpleZeroShotNL2Q
-from mintq.agenthub.sql_agent import SQLAgent, SQLAgentConfig
+from mintq.agenthub.simple_zero_shot import SimpleZeroShotNL2Q, SimpleZeroShotNL2QConfig
+from mintq.agenthub.sql_agent import SQLAgent
 from mintq.agenthub.ambig_simple import AmbigSimpleSQLAgent
 from mintq.agenthub.ambig_flat import AmbigFlatSQLAgent
 from mintq.agenthub.ambig_structured import AmbigStructuredSQLAgent
+from mintq.agenthub.utils import BasicAgentConfig
 
 __all__ = [
     "BaseSimpleSQLAgent",
     "BaseAmbigSQLAgent",
     "NL2QAgent",
     "BaseAgentConfig",
+    "BasicAgentConfig",
     "SimpleZeroShotNL2Q",
+    "SimpleZeroShotNL2QConfig",
     "SQLAgent",
-    "SQLAgentConfig",
     "AmbigSimpleSQLAgent",
     "AmbigFlatSQLAgent",
     "AmbigStructuredSQLAgent",
