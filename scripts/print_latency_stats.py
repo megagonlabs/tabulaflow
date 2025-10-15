48bb347385f146f01860ad59478eacd51fbe331f
Yanlin Feng <yanlinf23@gmail.com>
2025-10-15 01:29:09 +0000

    Update script

--- scripts/print_latency_stats.py
+++ scripts/print_latency_stats.py
@@ -9,15 +9,19 @@ async def main() -> None:
     latency = []
     dataset_loader = dataset_registry.get_class("arcs")()
     dataset = await dataset_loader.get_split_async("dev")
-    db_connector: SQLConnector = dataset.db_connectors["retails"]
+    dataset.tasks = [task for task in dataset.tasks if task.qid in ["061"]]
     for task in dataset.tasks:
         for gq in task.gold_queries:
-            latency.append((gq.exec_result.latency_seconds, task.qid, gq.id, gq.query))
+            latency.append((gq.exec_result.latency_seconds, task, gq))
     latency = sorted(latency, key=lambda x: -x[0])
-    for latency, qid, id, query in latency[:10]:
-        print(f"{qid}-{id}: {latency:.2f} seconds")
-        print(query)
-        exec_result = await db_connector.run_query_async(query, timeout=None)
+    for latency, task, gq in latency[:10]:
+        print(f"{task.qid}-{gq.id}: {latency:.2f} seconds")
+        print(gq.query)
+        print(gq.parameter_values)
+        db_connector = dataset.db_connectors[task.db]
+        exec_result = await db_connector.run_query_async(gq.query, gq.parameter_values, timeout=None)
+        if exec_result.error:
+            print(exec_result.error)
         print(f"Real latency: {exec_result.latency_seconds:.2f} seconds")
         print()
 
