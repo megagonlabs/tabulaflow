11c0698963a16d75583141695e115229a2126692
Yanlin Feng <yanlinf23@gmail.com>
2025-10-15 23:14:40 +0000

    Update

--- scripts/test_sqlite_async.py
+++ scripts/test_sqlite_async.py
@@ -1,5 +1,6 @@
 import time
 import asyncio
+from typing import Any
 from mintq.datahub import dataset_registry
 from mintq.db_connector import SQLConnector
 from mintq.toolhub import RunQueryTool
@@ -28,15 +29,24 @@ invalid_query = """
 SELECT * FROM asdfasdfasdf;
 """
 
+query2 = """
+SELECT COUNT(DISTINCT t1.account_id)
+FROM trans t1
+JOIN trans t2
+ON t1.account_id = t2.account_id
+AND t2.date > t1.date
+AND julianday(t2.date) - julianday(t1.date) <= :days_threshold
+WHERE t2.balance > t1.balance * (1 + :percentage_increase);
+"""
 
 def print_current_time() -> None:
     print(f"Current time: {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 
-async def run_query(db_connector: SQLConnector, query: str, timeout: int) -> None:
+async def run_query(db_connector: SQLConnector, query: str, parameters: dict[str, Any], timeout: int) -> None:
     print_current_time()
     # t0 = time.time()
-    result = await db_connector.run_query_async(query, timeout=timeout)
+    result = await db_connector.run_query_async(query, parameters, timeout=timeout)
     print(result)
     print(result.latency_seconds)
     print_current_time()
@@ -47,15 +57,16 @@ async def main() -> None:
     # dataset = await dataset_loader.get_split_async("dev")
     # db_connector: SQLConnector = dataset.db_connectors["retails"]
     db_connectors = await dataset_loader.get_db_connectors_async("dev")
-    db_connector = list(db_connectors.values())[0]
-    db_connector._t_eng.dbms_semaphore = None
-    db_connector._t_eng.db_semaphore = None
-    run_query_tool = RunQueryTool(db_connector, timeout=1)
+    db_connector = db_connectors["retails"]
+    # db_connector._t_eng.dbms_semaphore = None
+    # db_connector._t_eng.db_semaphore = None
+    run_query_tool = RunQueryTool(db_connector, timeout=120)
     # await run_query(db_connector, query, 60)
     t0 = time.time()
-    # await asyncio.gather(*[run_query(db_connector, query, 60) for _ in range(10)])
-    result = await run_query_tool(invalid_query)
-    print(result)
+    params = {'days_threshold': 7, 'percentage_increase': 0.5}
+    await asyncio.gather(*[run_query(db_connector, query2, params, 120) for _ in range(16)])
+    # result = await run_query_tool(invalid_query)
+    # print(result)
     print(f"Total time taken: {time.time() - t0} seconds")
 
 
