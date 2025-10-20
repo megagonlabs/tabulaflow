e05d81291fe3c08efc5560f930d8748999a861d6
Yanlin Feng <yanlinf23@gmail.com>
2025-10-20 22:38:07 +0000

    Set default timeout to 90

--- mintq/toolhub/run_query.py
+++ mintq/toolhub/run_query.py
@@ -14,7 +14,7 @@ class RunQueryToolMetrics(BaseModel):
 class RunQueryTool:
     name: ClassVar = "run_query"
 
-    def __init__(self, db_connector: BaseSQLDBConnector, timeout: int | None = 120):
+    def __init__(self, db_connector: BaseSQLDBConnector, timeout: int | None = 90):
         self.db_connector = db_connector
         self.timeout = timeout
         self._metrics = RunQueryToolMetrics()
