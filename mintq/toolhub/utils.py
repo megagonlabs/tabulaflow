53de340d23022e43f36ff9de1acb466a9f146610
Yanlin Feng <yanlinf23@gmail.com>
2025-10-22 23:42:03 +0000

    Remove [parameters...

--- mintq/toolhub/utils.py
+++ mintq/toolhub/utils.py
@@ -35,5 +35,6 @@ def equals_ci(a: str | None, b: str | None) -> bool:
 
 def format_sqlalchemy_error_msg(error_msg: str) -> str:
     error_msg = re.sub(r"\[SQL:.*?\]", "", error_msg)
+    error_msg = re.sub(r"\[parameters:.*?\]", "", error_msg)
     error_msg = re.sub(r"\(Background on this error at: https://sqlalche\.me/e/\S+\)", "", error_msg)
     return error_msg.strip()
