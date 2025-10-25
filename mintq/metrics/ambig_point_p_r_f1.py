58c4b8714fd862726256d5ad067d4e19e3af7d69
Yanlin Feng <yanlinf23@gmail.com>
2025-10-25 00:28:45 +0000

    Metrics interpretation_{p,r,f1}

--- mintq/metrics/ambig_point_p_r_f1.py
+++ mintq/metrics/ambig_point_p_r_f1.py
@@ -1,3 +1,5 @@
+import json
+from re import I
 from typing import ClassVar, Any
 from pydantic import BaseModel
 from pydantic_ai import Agent
@@ -7,7 +9,7 @@ from mintq.metrics.base import metric_registry
 from mintq.schema import PredAmbiguityPoint, GoldAmbiguityPoint
 
 
-_SYSTEM_PROMPT = """
+AMBIG_POINT_MATCHING_SYSTEM_PROMPT = """
 You are a helpful AI database expert that can evaluate the predicted ambiguity points for a question about a database.
 For each gold ambiguity point, you need to find the matching predicted ambiguity point.
 - Your output should be a mapping of ambiguity point id from the gold list to the predicted list.
@@ -64,12 +66,71 @@ Output:
 === END OF EXAMPLE ===
 """
 
-_USER_PROMPT = """
+AMBIG_POINT_MATCHING_USER_PROMPT = """
 Question: {{question}}
 Gold ambiguity points:
-{{gold_aps | tojson(indent=2)}}
+{{gold_aps}}
 Predicted ambiguity points:
-{{pred_aps | tojson(indent=2)}}
+{{pred_aps}}
+"""
+
+
+INTERPRETATION_MATCHING_SYSTEM_PROMPT = """
+You are a helpful AI database expert that can evaluate the predicted interpretations for an ambiguity point in a question about a database.
+For each gold interpretation, you need to find the matching predicted interpretation.
+- Your output should be a mapping of interpretation id from the gold list to the predicted list.
+- An predicted interpretation can only be matched to one gold interpretation.
+- An predicted interpretation is considered matched if the interpretation is semantically equivalent.
+- If a gold interpretation has no corresponding predicted interpretation, set the value to None.
+
+=== START OF EXAMPLE ===
+Question: List all students with high GPA from NY.
+Ambiguity phrase: "NY"
+Gold interpretations:
+[
+  {
+    "id": "GOLD-0",
+    "interpretation": "New York City"
+  },
+  {
+    "id": "GOLD-1",
+    "interpretation": "New York State"
+  }
+]
+Predicted ambiguity points:
+[
+  {
+    "id": "PRED-0",
+    "interpretation": "from New York City"
+  },
+  {
+    "id": "PRED-1",
+    "interpretation": "from New York County"
+  }
+]
+
+Output:
+{
+  "matches": [
+    {
+      "gold_id": "GOLD-0",
+      "pred_id": "PRED-0"
+    },
+    {
+      "gold_id": "GOLD-1",
+      "pred_id": null
+    }
+}
+=== END OF EXAMPLE ===
+"""
+
+INTERPRETATION_MATCHING_USER_PROMPT = """
+Question: {{question}}
+Ambiguity phrase: "{{phrase}}"
+Gold interpretations:
+{{gold_interpretations}}
+Predicted interpretations:
+{{pred_interpretations}}
 """
 
 
@@ -98,6 +159,12 @@ class AmbigPointPRF1:
         else:
             raise ValueError(f"Unknown ambiguity point type: {ap.type}")
 
+    def _p_r_f1(self, n_overlap: int, n_pred: int, n_gold: int) -> tuple[float, float, float]:
+        p = n_overlap / n_pred if n_pred > 0 else 0.0
+        r = n_overlap / n_gold if n_gold > 0 else 0.0
+        f1 = 2 * p * r / (p + r) if p + r > 0 else 0.0
+        return p, r, f1
+
     async def compute_async(self, task: StructuredAmbigNL2QTaskOutput) -> dict[str, float]:
         pred_aps = [self._to_simple_dict(ap, "PRED") for ap in task.pred_ambiguity_points]
         gold_aps = [self._to_simple_dict(ap, "GOLD") for ap in task.gold_ambiguity_points]
@@ -122,16 +189,61 @@ class AmbigPointPRF1:
         agent = Agent[None, LLMOutput](
             model=self.llm,
             output_type=LLMOutput,
-            instructions=_SYSTEM_PROMPT,
+            instructions=AMBIG_POINT_MATCHING_SYSTEM_PROMPT,
         )
-        prompt = jinja2.Template(_USER_PROMPT).render(question=task.question, gold_aps=gold_aps, pred_aps=pred_aps)
+        prompt = jinja2.Template(AMBIG_POINT_MATCHING_USER_PROMPT).render(
+            question=task.question, gold_aps=json.dumps(gold_aps, indent=2), pred_aps=json.dumps(pred_aps, indent=2)
+        )
         result = await agent.run(prompt)
-        n_overlap = sum(1 for match in result.output.matches if match.pred_id is not None)
-        p = n_overlap / len(pred_aps) if pred_aps else 0.0
-        r = n_overlap / len(gold_aps) if gold_aps else 0.0
-        f1 = 2 * p * r / (p + r) if p + r > 0 else 0.0
+        matches = [match for match in result.output.matches if match.pred_id is not None]
+        ambig_point_p, ambig_point_r, ambig_point_f1 = self._p_r_f1(len(matches), len(pred_aps), len(gold_aps))
+
+        p_list = []
+        r_list = []
+        f1_list = []
+        for match in matches:
+            pred_ap = next(ap for ap in pred_aps if ap["id"] == match.pred_id)
+            gold_ap = next(ap for ap in gold_aps if ap["id"] == match.gold_id)
+            if not (pred_ap["type"] == "finite" and gold_ap["type"] == "finite"):
+                continue
+
+            gold_interpretations = [
+                {"id": f"GOLD-{i}", "interpretation": interpretation}
+                for i, interpretation in enumerate(gold_ap["interpretations"])
+            ]
+            pred_interpretations = [
+                {"id": f"PRED-{i}", "interpretation": interpretation}
+                for i, interpretation in enumerate(pred_ap["interpretations"])
+            ]
+
+            agent = Agent[None, LLMOutput](
+                model=self.llm,
+                output_type=LLMOutput,
+                instructions=INTERPRETATION_MATCHING_SYSTEM_PROMPT,
+            )
+            prompt = jinja2.Template(INTERPRETATION_MATCHING_USER_PROMPT).render(
+                question=task.question,
+                phrase=gold_ap["phrase"],
+                gold_interpretations=json.dumps(gold_interpretations, indent=2),
+                pred_interpretations=json.dumps(pred_interpretations, indent=2),
+            )
+            print(prompt)
+            result = await agent.run(prompt)
+            print(result.output)
+            matches = [match for match in result.output.matches if match.pred_id is not None]
+            p, r, f1 = self._p_r_f1(len(matches), len(pred_ap["interpretations"]), len(gold_ap["interpretations"]))
+            p_list.append(p)
+            r_list.append(r)
+            f1_list.append(f1)
+
+        interpretation_p = sum(p_list) / len(p_list) if p_list else 0.0
+        interpretation_r = sum(r_list) / len(r_list) if r_list else 0.0
+        interpretation_f1 = sum(f1_list) / len(f1_list) if f1_list else 0.0
         return {
-            "ambig_point_p": p,
-            "ambig_point_r": r,
-            "ambig_point_f1": f1,
+            "ambig_point_p": ambig_point_p,
+            "ambig_point_r": ambig_point_r,
+            "ambig_point_f1": ambig_point_f1,
+            "interpretation_p": interpretation_p,
+            "interpretation_r": interpretation_r,
+            "interpretation_f1": interpretation_f1,
         }
