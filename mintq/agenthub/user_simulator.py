a00a42d6561d3d648944e1e48fc02794d1eac457
Yanlin Feng <yanlinf23@gmail.com>
2025-11-03 21:43:06 +0000

    Fix user_simulator

--- mintq/agenthub/user_simulator.py
+++ mintq/agenthub/user_simulator.py
@@ -88,7 +88,7 @@ class UserSimulator:
                     "phrase": ap.phrase,
                     "interpretation": ap.interpretations[ap.intended_interpretation_idx]  # type: ignore
                     if ap.type == "finite"
-                    else f"{ap.parameter_name} is {ap.intended_parameter_value}",
+                    else f"{ap.intended_parameter_operator} {ap.intended_parameter_value} ({ap.parameter_name})",
                 }
                 for ap in task.gold_ambiguity_points
             ],
