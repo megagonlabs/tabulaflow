87b53d22f0eb0af3139fa11329e5d43add962adb
Yanlin Feng <yanlinf23@gmail.com>
2025-11-03 20:05:45 +0000

    Update exps

--- scripts/arcs/misc.py
+++ scripts/arcs/misc.py
@@ -15,10 +15,13 @@ EXP_DIRS = {
     # "90_gpt-4.1_simple": "output/90_gpt-4.1_simple/",
     # "90_gpt-4.1_flat": "output/90_gpt-4.1_flat/",
     # "90_gpt-4.1_structured": "output/90_gpt-4.1_structured/",
-    "91_gpt-4.1_simple": "output/91_gpt-4.1_simple/",
-    "91_gpt-4.1_simple_patience_ap": "output/91_gpt-4.1_simple_patience_ap/",
-    "92_gpt-4.1_structured": "output/92_gpt-4.1_structured/",
-    "92_gpt-4.1_flat": "output/92_gpt-4.1_flat/",
+    # "91_gpt-4.1_simple": "output/91_gpt-4.1_simple/",
+    # "91_gpt-4.1_simple_patience_ap": "output/91_gpt-4.1_simple_patience_ap/",
+    # "92_gpt-4.1_structured": "output/92_gpt-4.1_structured/",
+    # "92_gpt-4.1_flat": "output/92_gpt-4.1_flat/",
+    "94_gpt-4.1_simple": "output/94_gpt-4.1_simple/",
+    "94_gpt-4.1_structured": "output/94_gpt-4.1_structured/",
+    "94_gpt-4.1_flat": "output/94_gpt-4.1_flat/",
 }
 
 TALBE_FMT = "github"
@@ -31,12 +34,12 @@ for method, exp_dir in EXP_DIRS.items():
 
 
 def main():
-    exp_a = EXP_RESULTS["91_gpt-4.1_simple_patience_ap"]
-    exp_b = EXP_RESULTS["92_gpt-4.1_structured"]
+    exp_a = EXP_RESULTS["94_gpt-4.1_simple"]
+    exp_b = EXP_RESULTS["94_gpt-4.1_structured"]
 
     for task_a, task_b in zip(exp_a.tasks, exp_b.tasks):
         assert task_a.qid == task_b.qid
-        if task_a.eval_metrics["simple_ex"] > task_b.eval_metrics["simple_ex"]:
+        if task_a.eval_metrics["simple_ex"] < task_b.eval_metrics["simple_ex"] and len(task_a.gold_ambiguity_points) >= 3:
             print(task_a.qid)
 
 if __name__ == "__main__":
