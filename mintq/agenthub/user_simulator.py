0d9be43c95edbd1ea219692e7676232ba91a26a3
Yanlin Feng <yanlinf23@gmail.com>
2025-11-07 02:20:01 +0000

    Two-stage  user simulator

--- mintq/agenthub/user_simulator.py
+++ mintq/agenthub/user_simulator.py
@@ -1,8 +1,8 @@
 from contextlib import asynccontextmanager
-from typing import Any, AsyncGenerator
+from typing import Any, AsyncGenerator, Literal
 from pydantic import BaseModel
 import pydantic_ai
-from pydantic_ai import Agent, ModelRetry, ToolOutput
+from pydantic_ai import Agent, ToolOutput
 import asyncio
 import jinja2
 from mintq.agenthub.base import (
@@ -17,42 +17,38 @@ from mintq.agenthub.base import (
 )
 from mintq.schema import AmbigNL2QTask, Usage, Trajectory
 
-USER_SIMULATOR_SYSTEM_PROMPT = """
+STAGE_1_SYSTEM_PROMPT = """
 You are a data analyst trying to solve the following task: {{task}}
 Here,{% for ap in ambig_points %}
 - [{{ap.id}}] "{{ap.phrase}}" should be interpreted as "{{ap.interpretation}}".{% endfor %}
 
-You will be asked a question regarding the possible ambiguities in the task, and you need to provide clarifications or reject the questions based on the rules below:
-- To answer the question, call the `end_with_answer` tool.
-- To reject the question, call the `end_with_reject` tool.
-
-For all questions, first identify whether there is a relevant ambiguity point provided above for the question:
-- If there is no relevant ambiguity point, reject the question.
-- If the question is not related to ambiguity clarification, reject the question.
-
-For "free_text" questions, you need to return the relevant ambiguity point id:
+You will be asked a question regarding the possible ambiguities in the task, you must identify the relevant ambiguity point id in the `relevant_ambiguity_point_id` field.
+- If there is no matching ambiguity point, set the `relevant_ambiguity_point_id` to None.
 - If multiple questions are asked, only identify the relevant ambiguity point for the first question and ignore the rest.
-
-For "multiple_choice" questions, you need to select from the given options:
-- If none of the options are correct, reject the question.
-
-For "value" questions, you need to select a value as well as an operator from the given options:
-- The data type of the value should be consistent with the one specified in the question.
-- If no valid value or no valid operator is correct, reject the question.
+- If the question is not related to ambiguity clarification, set the `relevant_ambiguity_point_id` to None.
 """.strip()
 
 
-ANSWER_FREE_TEXT_SYSTEM_PROMPT = """
+STAGE_2_SYSTEM_PROMPT = """
 You are a data analyst trying to solve the following task: {{task}}
 Here,{% for ap in ambig_points %}
 - [{{ap.id}}] "{{ap.phrase}}" should be interpreted as "{{ap.interpretation}}".{% endfor %}
 
 You will be asked a question regarding the possible ambiguities in the task, and you are responsible for providing clarifications using the above information.
+
+For "free_text" questions, you need to provide a free-text answer:
+- If multiple questions are asked, only answer the first question and say "Please only ask one question at a time."
 - Only answer what you are asked using only the information provided above. Do not include additional information.
 - Do not make assumptions beyond the provided information.
 - You should not ask questions.
-- If multiple questions are asked, only answer the first one and say "Please only ask one question at a time."
 - Your answer should be grammatical and linguistically diverse.
+
+For "multiple_choice" questions, you need to select from the given options:
+- If none of the options are correct, set answer number to null. 
+
+For "value" questions, you need to select a value as well as an operator from the given options:
+- The data type of the value should be consistent with the one specified in the question.
+- If no valid value or no valid operator is correct, set both fields to null.
 """.strip()
 
 
@@ -70,36 +66,23 @@ class UserSimulatorConfig(BaseModel):
     include_history: bool = True
 
 
-def get_answer_tool(output_type_or_func: Any) -> ToolOutput:
-    return ToolOutput(output_type_or_func, name="end_with_answer", description="End the conversation with an answer")
-
-
-def get_reject_tool() -> ToolOutput:
-    def end_with_reject() -> None:
-        return None
-
-    return ToolOutput(
-        end_with_reject, name="end_with_reject", description="End the conversation by rejecting the question"
-    )
-
-
 class UserSimulator:
     def __init__(self, config: UserSimulatorConfig):
         self.config = config
 
-        system_prompt = jinja2.Template(USER_SIMULATOR_SYSTEM_PROMPT).render(
+        stage_1_system_prompt = jinja2.Template(STAGE_1_SYSTEM_PROMPT).render(
             task=self.config.task,
             ambig_points=[ap.model_dump() for ap in self.config.ambig_points],
         )
 
-        self.user_agent = Agent(
+        self.stage_1_agent = Agent(
             model=self.config.llm,
             tools=[],
-            instructions=system_prompt,
+            instructions=stage_1_system_prompt,
             model_settings={"temperature": self.config.temperature},
         )
         self._message_history: list[pydantic_ai.messages.ModelMessage] = [
-            pydantic_ai.messages.ModelRequest(parts=[], instructions=system_prompt)
+            pydantic_ai.messages.ModelRequest(parts=[], instructions=stage_1_system_prompt)
         ]
         self._usage = Usage.create(llm=self.config.llm)
         self._lock = asyncio.Lock() if self.config.include_history else None
@@ -154,76 +137,79 @@ class UserSimulator:
         else:
             yield
 
-    async def ask_free_text_async(self, question: UserFreeTextQuestion) -> UserFreeTextAnswer | None:
-        def answer(relevant_ambig_point_id: str) -> str:
+    async def _identify_relevant_ambig_point_id_async(self, question_str: str) -> NLAmbigPoint | None:
+        def identify(relevant_ambig_point_id: str | None) -> str | None:
             """
             Args:
-                relevant_ambig_point_id: The id (e.g. "A", "B", etc.) of the ambiguity point that the question is asking about.
+                relevant_ambig_point_id: The id (e.g. "A", "B", etc.) of the ambiguity point that the question is asking about. If there is no match, set this to null.
             """
             return relevant_ambig_point_id
 
+        result = await self.stage_1_agent.run(  # type: ignore
+            question_str,
+            output_type=[ToolOutput(identify, name="identify")],
+            message_history=self._message_history if self.config.include_history else None,
+        )
+        self._usage += Usage.from_pydantic_ai_usage(result.usage(), self.config.llm)
+        self._message_history += result.new_messages()
+        relevant_ambig_point_id = result.output
+        if relevant_ambig_point_id is None:
+            return None
+        return next((ap for ap in self.config.ambig_points if ap.id == relevant_ambig_point_id), None)
+
+    async def _run_async(self, question_str: str, output_type_or_func: Any) -> UserAnswer | None:
         async with self._lock_message_history_async():
-            result0 = await self.user_agent.run(  # type: ignore
-                question.question,
-                output_type=[
-                    get_answer_tool(answer),
-                    get_reject_tool(),
-                ],
-                message_history=self._message_history if self.config.include_history else None,
-            )
-            self._usage += Usage.from_pydantic_ai_usage(result0.usage(), self.config.llm)
-            self._message_history += result0.new_messages()
-
-            relevant_ambig_point_id = result0.output
-
-            if relevant_ambig_point_id is None or not any(
-                ap.id == relevant_ambig_point_id for ap in self.config.ambig_points
-            ):
+            relevant_ambig_point = await self._identify_relevant_ambig_point_id_async(question_str)
+            if relevant_ambig_point is None:
                 return None
 
-            relevant_ambig_point = next(ap for ap in self.config.ambig_points if ap.id == relevant_ambig_point_id)
-            system_prompt = jinja2.Template(ANSWER_FREE_TEXT_SYSTEM_PROMPT).render(
+            stage_2_system_prompt = jinja2.Template(STAGE_2_SYSTEM_PROMPT).render(
                 task=self.config.task,
                 ambig_points=[relevant_ambig_point.model_dump()],
             )
-            answer_agent = Agent(
+            stage_2_agent: Agent[None, UserAnswer | None] = Agent(
                 model=self.config.llm,
-                instructions=system_prompt,
+                instructions=stage_2_system_prompt,
+                output_type=ToolOutput(output_type_or_func, name="answer"),
                 model_settings={"temperature": self.config.temperature},
             )
-            result = await answer_agent.run(
-                question.question, output_type=ToolOutput(UserFreeTextAnswer, name="answer")
-            )
+
+            result = await stage_2_agent.run(question_str)
             self._usage += Usage.from_pydantic_ai_usage(result.usage(), self.config.llm)
-            self._message_history += result.new_messages()[1:]  # The question is already added in the first step
+            self._message_history += result.new_messages()[1:]
         return result.output
 
+    async def ask_free_text_async(self, question: UserFreeTextQuestion) -> UserFreeTextAnswer | None:
+        return await self._run_async(question.question, UserFreeTextAnswer)  # type: ignore
+
     async def ask_multiple_choice_async(self, question: UserMultipleChoiceQuestion) -> UserMultipleChoiceAnswer | None:
-        def answer(number: int) -> UserMultipleChoiceAnswer:
-            if number < 1 or number > len(question.options):
-                raise ModelRetry(f"Answer number should be between 1 and {len(question.options)}")
+        def answer(number: int | None) -> UserMultipleChoiceAnswer | None:
+            """
+            Args:
+                number: The number of the selected option. If none of the options are correct, set this to null.
+            """
+            if number is None or number < 1 or number > len(question.options):
+                return None
             return UserMultipleChoiceAnswer(answer_index=number - 1)
 
-        async with self._lock_message_history_async():
-            result = await self.user_agent.run(  # type: ignore
-                question.question + "".join([f"\n[{i + 1}] {o}" for i, o in enumerate(question.options)]),
-                output_type=[get_answer_tool(answer), get_reject_tool()],
-                message_history=self._message_history if self.config.include_history else None,
-            )
-            self._usage += Usage.from_pydantic_ai_usage(result.usage(), self.config.llm)
-            self._message_history += result.new_messages()
-        return result.output  # type: ignore
+        question_str = question.question + "".join([f"\n[{i + 1}] {o}" for i, o in enumerate(question.options)])
+        return await self._run_async(question_str, answer)  # type: ignore
 
     async def ask_value_async(self, question: UserValueQuestion) -> UserValueAnswer | None:
-        async with self._lock_message_history_async():
-            result = await self.user_agent.run(  # type: ignore
-                question.model_dump_json(indent=2),
-                output_type=[get_answer_tool(UserValueAnswer), get_reject_tool()],
-                message_history=self._message_history if self.config.include_history else None,
-            )
-            self._usage += Usage.from_pydantic_ai_usage(result.usage(), self.config.llm)
-            self._message_history += result.new_messages()
-        return result.output  # type: ignore
+        def answer(
+            operator: Literal["<", ">", "<=", ">=", "=", "<>"] | None, value: int | float | str | None
+        ) -> UserValueAnswer | None:
+            """
+            Args:
+                operator: The operator of the value to use in "<expression> <operator> <value>". If none of the operators are correct, set this to null.
+                value: The intended value of the parameter. If no valid value is correct, set this to null.
+            """
+            if operator is None or value is None:
+                return None
+            return UserValueAnswer(operator=operator, value=value)
+
+        question_str = question.model_dump_json(indent=2)
+        return await self._run_async(question_str, answer)  # type: ignore
 
     async def ask_async(self, question: UserQuestion) -> UserAnswer | None:
         if question.type == "free_text":
