d8cf6b1c7baf6230fd57102bfbf1917490fcf1fc
Yanlin Feng <yanlinf23@gmail.com>
2025-11-18 00:47:37 +0000

    get_schema only for disambiguation

--- mintq/agenthub/ambig_structured.py
+++ mintq/agenthub/ambig_structured.py
@@ -185,7 +185,7 @@ class AmbigStructuredSQLAgent:
                 language=ctx.task.language, dataset_instructions=ctx.task.dataset_instructions
             ),
             output_type=LLMOutput,
-            tool_keys=["get_schema", "get_column_description"],
+            tool_keys=["get_schema"],
         )
         prompt = f"List all ambiguity points: {ctx.task.question}"
 
