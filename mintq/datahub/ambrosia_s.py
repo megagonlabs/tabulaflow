0389ed9c31ffdc80ab4c20830ad96c19d0576aac
Yanlin Feng <yanlinf23@gmail.com>
2025-11-19 22:20:29 +0000

    Revise prompt

--- mintq/datahub/ambrosia_s.py
+++ mintq/datahub/ambrosia_s.py
@@ -42,6 +42,7 @@ AMBROSIA_TAXONOMY = """
 
 AMBROSIA_DATASET_INSTRUCTIONS = """
 - Do not concatenate columns in the results unless explicitly requested.
+- Each question has exactly one ambiguity point.
 - There are no parameter ambiguity points.
 """.strip()
 
