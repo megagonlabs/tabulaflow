de07683daa09a139f0b1771158da21a586310d7b
Yanlin Feng <yanlinf23@gmail.com>
2025-11-20 01:28:35 +0000

    analyze_ambrosia.py

---
+++ scripts/analyze_ambrosia.py
@@ -0,0 +1,13 @@
+import os
+from mintq.schema import NL2QRunResult
+
+
+result_dir = "output/115_o4-mini-structured/"
+
+
+with open(os.path.join(result_dir, "result.json"), "r") as f:
+    result = NL2QRunResult.model_validate_json(f.read())
+
+for task in result.tasks:
+    if task.eval_metrics["simple_ex"] == 0.0:
+        print(task.qid, task.extra_info["ambrosia"]["ambig_type"])
