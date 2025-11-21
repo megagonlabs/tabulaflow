2d15df50c355599d532b9d8160a5c726f0945e12
Yanlin Feng <yanlinf23@gmail.com>
2025-11-21 01:34:40 +0000

    print_exp

--- scripts/arcs/print_exp.py
+++ scripts/arcs/print_exp.py
@@ -3,47 +3,24 @@ from typing import Any
 from mintq.schema import AmbigNL2QTask, NL2QRunResult
 from decimal import Decimal
 from tabulate import tabulate
+import time
 
 
 EXP_DIRS = {
-    # "structured_gpt-oss-120b": "output/87_gpt-oss-120b/",
-    # "structured_claude-sonnet-4-5-20250929": "output/85_claude-sonnet-4-5-20250929/",
-    # "structured_gemini-2.0-flash": "output/84_gemini-2.0-flash/",
-    # "structured_gpt-4.1-mini": "output/82_openai-responses:gpt-4.1-mini/",
-    # "structured_gpt-4.1": "output/82_openai-responses:gpt-4.1/",
-    # "90_gpt-4.1_simple": "output/90_gpt-4.1_simple/",
-    # "90_gpt-4.1_flat": "output/90_gpt-4.1_flat/",
-    # "90_gpt-4.1_structured": "output/90_gpt-4.1_structured/",
-    # "91_gpt-4.1_simple": "output/91_gpt-4.1_simple/",
-    # "91_gpt-4.1_simple_patience_ap": "output/91_gpt-4.1_simple_patience_ap/",
-    # "93_gpt-4.1_structured": "output/93_gpt-4.1_structured/",
-    # "93_gpt-4.1_flat": "output/93_gpt-4.1_flat/",
-    # "94_gpt-4.1_simple": "output/94_gpt-4.1_simple/",
-    # "94_gpt-4.1_structured": "output/94_gpt-4.1_structured/",
-    # "94_gpt-4.1_flat": "output/94_gpt-4.1_flat/",
-    # "95_gpt-4.1_simple": "output/95_gpt-4.1_simple/",
-    # "95_gpt-4.1_simple_patience_ap": "output/95_gpt-4.1_simple_patience_ap/",
-    # "95_gpt-4.1_structured": "output/95_gpt-4.1_structured/",
-    # "95_gpt-4.1_flat": "output/95_gpt-4.1_flat/",
-    "97_gpt-4.1_simple": "output/97_gpt-4.1_simple/",
-    "97_gpt-4.1_simple_patience_ap": "output/97_gpt-4.1_simple_patience_ap/",
-    "97_gpt-4.1_structured": "output/97_gpt-4.1_structured/",
-    "97_gpt-4.1_flat": "output/97_gpt-4.1_flat/",
-    "98_gemini_2.0_flash_simple": "output/98_gemini_2.0_flash_simple/",
-    "98_gemini_2.0_flash_simple_patience_ap": "output/98_gemini_2.0_flash_simple_patience_ap/",
-    "98_gemini_2.0_flash_structured": "output/98_gemini_2.0_flash_structured/",
-    "98_gemini_2.0_flash_flat": "output/98_gemini_2.0_flash_flat/",
-    "99_gpt-4.1_simple_patience_0": "output/99_gpt-4.1_simple_patience_0/",
-    "99_gpt-4.1_simple_patience_1": "output/99_gpt-4.1_simple_patience_1/",
+    "o4-mini-medium_simple": "output/130_o4-mini-medium-simple/",
+    "o4-mini-medium_structured": "output/130_o4-mini-medium_structured/",
 }
 
 TALBE_FMT = "github"
 
 
-EXP_RESULTS = {}
+EXP_RESULTS: dict[str, NL2QRunResult] = {}
+
+t0 = time.time()
 for method, exp_dir in EXP_DIRS.items():
     with open(os.path.join(exp_dir, "result.json"), "r") as f:
         EXP_RESULTS[method] = NL2QRunResult.model_validate_json(f.read())
+print(f"Loaded {len(EXP_RESULTS)} results in {time.time() - t0:.2f} seconds")
 
 
 def calc_average(values: list[float]) -> float:
@@ -66,19 +43,27 @@ def num_aps(task: AmbigNL2QTask, finite_only: bool = False) -> int:
     return len([ap for ap in task.gold_ambiguity_points if not finite_only or ap.type == "finite"])
 
 
-def print_main_table(exp_names: list[str]):
+def print_main_table():
+    exp_names = [
+        "o4-mini-medium_simple",
+        "o4-mini-medium_structured",
+    ]
     headers = ["Method", "EX", "EX_1AP", "EX_2AP", "EX_3+AP", "User Effort", "Latency", "Cost"]
     rows = []
     for exp_name in exp_names:
         result = EXP_RESULTS[exp_name]
-        ex = result.aggregated_eval_metrics["simple_ex"]["avg"]
-        ex_1ap = calc_average([task.eval_metrics["simple_ex"] for task in result.tasks if num_aps(task) == 1])
-        ex_2ap = calc_average([task.eval_metrics["simple_ex"] for task in result.tasks if num_aps(task) == 2])
-        ex_3plusap = calc_average([task.eval_metrics["simple_ex"] for task in result.tasks if num_aps(task) >= 3])
-        user_effort = result.total_user_simulator_usage.output_tokens / len(result.tasks)
-        latency = result.aggregated_inference_metrics["latency_seconds"]["avg"]
-        cost = result.total_usage.api_cost_usd / len(result.tasks)
-        rows.append([exp_name, ex, ex_1ap, ex_2ap, ex_3plusap, user_effort, latency, cost])
+        rows.append(
+            [
+                exp_name,
+                result.aggregated_eval_metrics["simple_ex"]["avg"],
+                result.aggregated_eval_metrics["simple_ex_by_ambig_point_num"]["1AP"]["avg"],
+                result.aggregated_eval_metrics["simple_ex_by_ambig_point_num"]["2AP"]["avg"],
+                result.aggregated_eval_metrics["simple_ex_by_ambig_point_num"]["3+AP"]["avg"],
+                result.aggregated_inference_metrics["user_effort"]["avg"],
+                result.aggregated_inference_metrics["latency_seconds"]["avg"],
+                result.total_usage.api_cost_usd / len(result.tasks),
+            ]
+        )
     print_table("Main Table", headers, rows)
 
 
@@ -150,11 +135,9 @@ def print_result_by_ambiguity_type(exp_names: list[str]):
 
 
 def main():
-    # print_main_table(exp_results)
-    all_exps = list(EXP_DIRS.keys())
-    print_main_table(all_exps)
-    print_main_table_finite_ap(all_exps)
-    print_user_effort_table(all_exps)
+    print_main_table()
+    # print_main_table_finite_ap(all_exps)
+    # print_user_effort_table(all_exps)
     # print_result_by_ambiguity_type(exp_results)
 
 
