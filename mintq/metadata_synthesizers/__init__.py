136560ec74be19f721589e871386446ed7d5b30a
Yanlin Feng <yanlinf23@gmail.com>
2025-11-22 06:31:14 +0000

    Fix

--- mintq/metadata_synthesizers/__init__.py
+++ mintq/metadata_synthesizers/__init__.py
@@ -1,5 +1,4 @@
 from mintq.metadata_synthesizers.base import BaseSchemaCompressor
-from mintq.metadata_synthesizers.er_diagram import LLMERDiagramSynthesizer
 from mintq.metadata_synthesizers.schema_compressor import SchemaCompressor
 
-__all__ = ["BaseSchemaCompressor", "LLMERDiagramSynthesizer", "SchemaCompressor"]
+__all__ = ["BaseSchemaCompressor", "SchemaCompressor"]
