363e42636ac8e3a7e72c1599dea931dc671eb129
Yanlin Feng <yanlinf23@gmail.com>
2026-01-13 00:00:12 +0000

    scripts/find_gold_not_executable.py

---
+++ scripts/find_gold_not_executable.py
@@ -0,0 +1,33 @@
+import asyncio
+import argparse
+from tqdm.asyncio import tqdm_asyncio
+from mintq.datahub import dataset_registry
+from mintq.pipelines.populate_exec_results import populate_task_async
+
+
+async def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default="bird-sql")
+    parser.add_argument("--split", default="dev_20240627")
+    args = parser.parse_args()
+    print(args)
+    print()
+
+    dataset_loader = dataset_registry.get_class(args.dataset)()
+    dataset = await dataset_loader.get_split_async(args.split)
+
+    await tqdm_asyncio.gather(*[populate_task_async(task, dataset.db_connectors[task.db]) for task in dataset.tasks])
+
+    num_not_executable = 0
+    for task in dataset.tasks:
+        if task.gold_query.exec_result.error is not None:
+            num_not_executable += 1
+            print()
+            print(f"===== QID: {task.qid} =====\n")
+            print(task.gold_query.to_readable())
+
+    print(f"Number of not executable gold queries: {num_not_executable}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
