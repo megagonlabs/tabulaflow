5f2bfda5658f9ad3b0e8cd0c3456e7f480b85f73
Yanlin Feng <yanlinf23@gmail.com>
2026-01-13 00:16:51 +0000

    Fix

--- scripts/find_gold_not_executable.py
+++ scripts/find_gold_not_executable.py
@@ -1,5 +1,6 @@
 import asyncio
 import argparse
+import time
 from tqdm.asyncio import tqdm_asyncio
 from mintq.datahub import dataset_registry
 from mintq.pipelines.populate_exec_results import populate_task_async
@@ -13,10 +14,16 @@ async def main() -> None:
     print(args)
     print()
 
+    t0 = time.time()
     dataset_loader = dataset_registry.get_class(args.dataset)()
     dataset = await dataset_loader.get_split_async(args.split)
+    print(
+        f"Loaded {len(dataset.tasks)} tasks and {len(dataset.db_connectors)} databases from {args.dataset} ({args.split}) in {time.time() - t0:.2f} seconds."
+    )
 
-    await tqdm_asyncio.gather(*[populate_task_async(task, dataset.db_connectors[task.db]) for task in dataset.tasks])
+    await tqdm_asyncio.gather(
+        *[populate_task_async(task, dataset.db_connectors[task.db], force=True) for task in dataset.tasks]
+    )
 
     num_not_executable = 0
     for task in dataset.tasks:
@@ -33,7 +40,7 @@ async def main() -> None:
                 print()
                 print()
                 print(f"### QID: {task.qid}")
-                print(task.gold_query.to_readable())
+                print(query.to_readable())
 
     print(f"Number of not executable gold queries: {num_not_executable}")
 
