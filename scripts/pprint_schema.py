3d7c2fd886de4d3ca9cb1cead6148540363404e0
Yanlin Feng <yanlinf23@gmail.com>
2026-01-22 06:25:53 +0000

    add_description=True by default

--- scripts/pprint_schema.py
+++ scripts/pprint_schema.py
@@ -9,6 +9,7 @@ async def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_path", default="cache/schemas/spider2-snow+GITHUB_REPOS_DATE.json")
     parser.add_argument("--compress", action="store_true")
+    parser.add_argument("--no_description", action="store_true")
     args = parser.parse_args()
     print(args)
     print()
@@ -20,7 +21,7 @@ async def main() -> None:
         schema = await SchemaCompressor().run_async(schema)
 
     formatter = SQLDefaultSchemaFormatter()
-    schema_str = formatter.format(schema)
+    schema_str = formatter.format(schema, add_description=not args.no_description)
     print(schema_str)
     print()
     print(f"(schema length: {len(schema_str)} characters)")
