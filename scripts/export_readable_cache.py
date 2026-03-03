e61e8e84236f0aea55138af1d911f563c6168dfa
Yanlin Feng <yanlinf23@gmail.com>
2026-03-03 22:59:59 +0000

    Update

--- scripts/export_readable_cache.py
+++ scripts/export_readable_cache.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from mintq.config import mintq_config
+from mintq.preprocessors.db_summarizer import DBSummary
 from mintq.schema import SQLSchema
 from mintq.preprocessors.er_diagram import ERDiagram
 from mintq.formatters.sql_ddl import SQLDDLSchemaFormatter
@@ -12,6 +13,25 @@ def main() -> None:
     parser.add_argument("--output_dir", default="readable/")
     args = parser.parse_args()
 
+    input_dir = os.path.join(mintq_config.cache_dir, "schemas")
+    output_dir = os.path.join(args.output_dir, "schemas")
+    os.makedirs(output_dir, exist_ok=True)
+    for f in os.listdir(input_dir):
+        schema = SQLSchema.model_validate_json(open(os.path.join(input_dir, f)).read())
+        schema_str = SQLDDLSchemaFormatter().format(schema, add_description=True)
+        with open(os.path.join(output_dir, f.replace(".json", ".md")), "w") as f:
+            f.write(schema_str)
+    print(f"Exported {len(os.listdir(input_dir))} schemas to {output_dir}")
+
+    input_dir = os.path.join(mintq_config.cache_dir, "preprocessors", "db_summarizer")
+    output_dir = os.path.join(args.output_dir, "preprocessors", "db_summarizer")
+    os.makedirs(output_dir, exist_ok=True)
+    for f in os.listdir(input_dir):
+        summary = DBSummary.model_validate_json(open(os.path.join(input_dir, f)).read())
+        with open(os.path.join(output_dir, f.replace(".json", ".md")), "w") as f:
+            f.write(summary.summary_markdown)
+    print(f"Exported {len(os.listdir(input_dir))} DB summaries to {output_dir}")
+
     input_dir = os.path.join(mintq_config.cache_dir, "preprocessors", "schema_preprocessor")
     output_dir = os.path.join(args.output_dir, "preprocessors", "schema_preprocessor")
     os.makedirs(output_dir, exist_ok=True)
@@ -20,7 +40,7 @@ def main() -> None:
         schema_str = SQLDDLSchemaFormatter().format(schema, add_description=True)
         with open(os.path.join(output_dir, f.replace(".json", ".md")), "w") as f:
             f.write(schema_str)
-    print(f"Exported {len(os.listdir(input_dir))} schemas to {output_dir}")
+    print(f"Exported {len(os.listdir(input_dir))} preprocessed schemas to {output_dir}")
 
     input_dir = os.path.join(mintq_config.cache_dir, "preprocessors", "er_diagram_synthesizer")
     output_dir = os.path.join(args.output_dir, "preprocessors", "er_diagram_synthesizer")
@@ -31,16 +51,6 @@ def main() -> None:
         with open(os.path.join(output_dir, f.replace(".json", ".md")), "w") as f:
             f.write(er_diagram_str)
     print(f"Exported {len(os.listdir(input_dir))} ER diagrams to {output_dir}")
-
-    input_dir = os.path.join(mintq_config.cache_dir, "schemas")
-    output_dir = os.path.join(args.output_dir, "schemas")
-    os.makedirs(output_dir, exist_ok=True)
-    for f in os.listdir(input_dir):
-        schema = SQLSchema.model_validate_json(open(os.path.join(input_dir, f)).read())
-        schema_str = SQLDDLSchemaFormatter().format(schema, add_description=True)
-        with open(os.path.join(output_dir, f.replace(".json", ".md")), "w") as f:
-            f.write(schema_str)
-    print(f"Exported {len(os.listdir(input_dir))} schemas to {output_dir}")
 
 
 if __name__ == "__main__":
