50631071ff179b5042db738e928fe73f50b46194
Yanlin Feng <yanlinf23@gmail.com>
2026-03-27 06:13:10 +0000

    Grid color

--- mintq/toolhub/render_chart.py
+++ mintq/toolhub/render_chart.py
@@ -83,10 +83,8 @@ def render_plotext(
     plt.theme("dark")
     plt.plotsize(width, None)
 
-    from mintq.cli.theme import ACCENT_DIM_RGB
-
     plt.axes_color("default")
-    plt.ticks_color(ACCENT_DIM_RGB)
+    plt.ticks_color("default")
 
     x_data = df[x_field].tolist()
     y_data = df[y_field].tolist()
