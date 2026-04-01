360886017acde871ddef7a336ccbc5ac18ca2749
Yanlin Feng <yanlinf23@gmail.com>
2026-04-01 06:14:00 +0000

    DBRegistry

--- mintq/db_connector/__init__.py
+++ mintq/db_connector/__init__.py
@@ -1,10 +1,12 @@
 from mintq.db_connector.base import BasePropertyGraphDBConnector, BaseSQLDBConnector, NL2QDBConnector
+from mintq.db_connector.db_registry import DBRegistry
 from mintq.db_connector.neo4j_conn import Neo4jConnector
 from mintq.db_connector.sql_conn import DATA_FILE_EXTENSIONS, SQLConnector
 
 __all__ = [
     "BasePropertyGraphDBConnector",
     "BaseSQLDBConnector",
+    "DBRegistry",
     "DATA_FILE_EXTENSIONS",
     "NL2QDBConnector",
     "Neo4jConnector",
