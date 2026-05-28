b6f23ae8ed3c4e50fa667f084253624242aeb884
Yanlin Feng <yanlinf23@gmail.com>
2026-05-28 16:35:12 -0700

    Render absolute url

--- mintq/toolhub/web_browser.py
+++ mintq/toolhub/web_browser.py
@@ -185,7 +185,7 @@ async def take_snapshot(page: "Page") -> PageSnapshot:
     return PageSnapshot(
         url=page.url,
         title=title,
-        markdown_content=render_aria_markdown(aria_yaml),
+        markdown_content=render_aria_markdown(aria_yaml, base_url=page.url),
         refs=extract_refs(aria_yaml),
         aria_yaml=aria_yaml,
     )
